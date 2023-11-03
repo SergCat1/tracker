368c474b17326d0fff591c419017204af0b8f7b0
Сергей Хрипко <khripko.ser@yandex.ru>
2023-11-03 16:07:14 +0300

    7.7 Взаимодействие объектов

--- src/main/java/ru/oop/Ball.java
+++ src/main/java/ru/oop/Ball.java
@@ -4,7 +4,7 @@ public class Ball {
     public void tryRun(boolean condition) {
         if (condition) {
             System.out.println("Колобок съеден");
-        } else if (!condition) {
+        } else {
             System.out.println("Колобок убежал");
         }
 
