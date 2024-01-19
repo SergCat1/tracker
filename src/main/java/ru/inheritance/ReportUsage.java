72f86095bdedd91dd991aa2b976d235459a57f3b
Hripko <khripko.ser@yandex.ru>
2024-01-19 18:34:47 +0300

    8.5 Вызвать переопределенный метод super

---
+++ src/main/java/ru/inheritance/ReportUsage.java
@@ -0,0 +1,8 @@
+package ru.inheritance;
+public class ReportUsage {
+    public static void main(String[] args) {
+        JSONReport report = new JSONReport();
+        String text = report.generate("Report's name", "Report's body");
+        System.out.println(text);
+    }
+}
