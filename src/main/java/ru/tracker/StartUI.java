1c64eeb42c2a563c6ae6276f44521980f36fec81
Hripko <khripko.ser@yandex.ru>
2024-02-08 10:34:16 +0300

    8.6 Аннотация @Override

---
+++ src/main/java/ru/tracker/StartUI.java
@@ -0,0 +1,13 @@
+package ru.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item show = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = show.getCreated().format(formatter);
+        System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+
+    }
+}
