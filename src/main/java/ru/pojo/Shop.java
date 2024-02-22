0ebb94e01a5c21ded10352ab8e37776b2a83f6c7
Hripko <khripko.ser@yandex.ru>
2024-02-22 18:00:35 +0300

    9.4 Массив c пустыми ячейками

--- src/main/java/ru/pojo/Shop.java
+++ src/main/java/ru/pojo/Shop.java
@@ -1,9 +1,11 @@
 package ru.pojo;
 
 public class Shop {
-    public static int indexOfNull(Product[] p) {
-        for (int i = 0; i < p.length; i++) {
-            if (p[i] == null) return i;
+    public static int indexOfNull(Product[] prodcut) {
+        for (int i = 0; i < prodcut.length; i++) {
+            if (prodcut[i] == null) {
+                return i;
+            }
         }
         return -1;
     }
