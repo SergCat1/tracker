fd3aab48d27fbcaf7196790ba640fadaa8f96e30
Hripko <khripko.ser@yandex.ru>
2024-03-11 18:49:46 +0300

    9.7 Хранилище

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,35 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+
+    public Item[] findByName(String key) {
+        int count = 0;
+
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+                count++;
+            }
+        }
+
+        Item[] rsl = new Item[count];
+        int newIndex = 0;
+
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+                rsl[newIndex] = item;
+                newIndex++;
+            }
+        }
+
+        return Arrays.copyOf(rsl, count);
     }
 
     public Item findById(int id) {
