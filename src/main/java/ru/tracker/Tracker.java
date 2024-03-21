b755b57fa412ff3f7bf12703dcc4ca43654e45d6
Hripko <khripko.ser@yandex.ru>
2024-03-21 18:05:15 +0300

    9.8 Метод удаления заявки

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -55,4 +55,16 @@ public class Tracker {
         }
         return result;
     }
+
+    public void delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            int start = index + 1;
+            int distPos = index;
+            int length = size - index - 1;
+            System.arraycopy(items, start, items, distPos, length);
+            items[size - 1] = null;
+            size--;
+        }
+    }
 }
