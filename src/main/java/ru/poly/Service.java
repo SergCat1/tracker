ba18b4d4e669a33b4fc6c78b9ddb13c0ff66d9e8
SergCat1 <hripko.s11@gmail.com>
2024-04-10 00:24:56 +0300

    ///

---
+++ src/main/java/ru/poly/Service.java
@@ -0,0 +1,19 @@
+package ru.poly;
+
+public class Service {
+    private Store store;
+
+    public Service(Store store) {
+        this.store = store;
+    }
+
+    public void add() {
+        store.save("Ivan Ivanov");
+    }
+
+    public static void main(String[] args) {
+        MemStore store = new MemStore();
+        Service service = new Service(store);
+        service.add();
+    }
+}
