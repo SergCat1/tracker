03e652fa244a034186d4442cefac7eb00d9ddfba
SergCat1 <hripko.s11@gmail.com>
2024-04-15 23:18:34 +0300

    10.13 Input

---
+++ src/main/java/ru/oop/College.java
@@ -0,0 +1,9 @@
+package ru.oop;
+
+public class College {
+    public static void main(String[] args) {
+        Freshman obj = new Freshman();
+        Student obj1 = obj;
+        Object obj2 = obj1;
+    }
+}
